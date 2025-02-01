55291b1a631aa6591b9bc1fc29f65b047a100e6d
vi <166890492+1moremystery@users.noreply.github.com>
2025-02-01 03:33:08 -0600

    changed interact to a raycast w/ length

---
+++ cargo_box.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+
+func doThing():
+	print("DONE!")
+	
+
+#queue_free()
--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED :float = 300.0
 const halfPi :float = PI/2
+const RAYLEN:int = 100
 
 func _physics_process(delta: float) -> void:
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -12,3 +13,14 @@ func _physics_process(delta: float) -> void:
 	rotate(halfPi);
 	
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
+		#shoot raycast
+		if(position.distance_to(get_global_mouse_position()) <=  RAYLEN):
+			var space_state:PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
+			var query = PhysicsRayQueryParameters2D.create(position,get_global_mouse_position())
+			var result = space_state.intersect_ray(query)
+			if(!result.is_empty()):
+				var hit = result.get("collider")
+				hit.doThing()
