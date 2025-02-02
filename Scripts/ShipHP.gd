2c7feff99a051bf73faea6c151890f505cf2669f
vi <166890492+1moremystery@users.noreply.github.com>
2025-02-01 23:17:41 -0600

    JSON on Outside shipmode

--- Scripts/ShipHP.gd
+++ Scripts/ShipHP.gd
@@ -5,8 +5,18 @@ extends Node
 @export var warningHealth:int = 30
 @export var ShipHP:int = 300
 
+var json:JSON = JSON.new()
+var error = json.parse(FileAccess.open("res://PLAYER_DATA.json", FileAccess.READ).get_as_text())
+static var PLAYERDATA
+
+func _ready() -> void:
+	if(error == OK):
+		PLAYERDATA = json.data
+		partHealths = PLAYERDATA["shipPartsHP"]
+		ShipHP = PLAYERDATA["shipHP"]
+		
+
 func DamagePart(partNum, Dmg = 10):
-	
 	partHealths[partNum]-=Dmg
 	if (partHealths[partNum]<=0):
 		print("reached")
@@ -29,4 +39,10 @@ func Repair(partNum): #repairs the thing only if it can
 	if(partHealths[partNum]>=0):
 		partHealths[partNum]=100
 
-	
+func _exit_tree() -> void:
+	PLAYERDATA["shipPartsHP"] = partHealths
+	PLAYERDATA["shipHP"] = ShipHP
+	var stringy:String = json.stringify(PLAYERDATA)
+	var file = FileAccess.open("res://PLAYER_DATA.json",FileAccess.WRITE)
+	file.store_string(stringy)
+	file.close()
